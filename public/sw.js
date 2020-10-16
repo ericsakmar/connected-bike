a91e966ae639f6061a5b5c50b8bb30015536e227
Eric Sakmar <esakmar@truefit.io>
2020-10-16 17:35:19 -0400

    tunes service worker

--- public/sw.js
+++ public/sw.js
@@ -1,20 +1,30 @@
-self.addEventListener("fetch", (event) => {
-  if (event.request.method !== "GET") {
-    return;
-  }
+const SESSIONS_URL = "https://www.googleapis.com/fitness/v1/users/me/sessions";
 
-  if (!event.request.url.startsWith("https://www.googleapis.com/fitness")) {
+self.addEventListener("fetch", (event) => {
+  if (
+    // only cache gets
+    event.request.method !== "GET" ||
+    // ...for google fit
+    !event.request.url.startsWith("https://www.googleapis.com/fitness") ||
+    // ...but not the sessions.
+    // TODO if you're feeling fancy some day, do network-first requets for sessions
+    event.request.url.startsWith(SESSIONS_URL)
+  ) {
     return;
   }
 
   event.respondWith(
     caches.match(event.request).then((cacheRes) => {
-      // TODO check age?
       if (cacheRes) {
         return cacheRes;
       }
 
       return fetch(event.request).then((res) => {
+        // don't cache unsuccessful requests
+        if (res.code !== 200) {
+          return res;
+        }
+
         return caches.open("v1").then((cache) => {
           cache.put(event.request, res.clone());
           return res;
